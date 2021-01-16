fbc5761e87a9dbd64fa8d2b983f5ea77a03fbe82
mark.tiahrt@concentrix.com <mark.tiahrt@outlook.com>
2021-01-16 16:48:48 -0700

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import bricking from "./images/brickwall.gif";
+import blueprints from "./images/blueprints.gif";
+import worker from './images/en-construction.gif';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Comming Soon!</h1>
+        <h2>gretchenkelly shop</h2>
+        <img className="blue-prints" src={blueprints}></img>
+        <img className="bricking" src={bricking}></img>
+        <img className="worker" src={worker}/>
       </header>
     </div>
   );
