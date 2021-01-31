e095b7f600afccf9e823edfa82bc0408d60e8e18
mark.tiahrt@concentrix.com <mark.tiahrt@outlook.com>
2021-01-31 15:36:21 -0700

    added start to nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
 import ItemList from "./Components/ItemList";
+import NavBar from "./Nav/NavBar";
+import NavItem from "./Nav/NavItem";
+import DropdownMenu from "./Nav/DropdownMenu";
+
+import { ReactComponent as BellIcon } from "./icons/bell.svg";
+import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
+import { ReactComponent as CaretIcon } from "./icons/caret.svg";
+import { ReactComponent as PlusIcon } from "./icons/plus.svg";
 
 function App() {
   return (
     <div className="App">
+      <NavBar>
+        <NavItem icon={<BellIcon />}></NavItem>
+        <NavItem icon={<PlusIcon />}> </NavItem>
+        <NavItem icon={<MessengerIcon />}> </NavItem>
+
+        <NavItem icon={<CaretIcon />}>
+          <DropdownMenu></DropdownMenu>
+        </NavItem>
+      </NavBar>
       <header>
         <h1>gretchenkelly shop</h1>
       </header>
