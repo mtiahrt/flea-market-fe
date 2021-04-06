7d4d841b1e83f90e68254d5108c4135d942e29a3
mark.tiahrt@concentrix.com <mark.tiahrt@outlook.com>
2021-04-06 13:32:13 -0600

    fixed deconstruction error

--- src/Components/NewItemList.js
+++ src/Components/NewItemList.js
@@ -20,21 +20,24 @@ const mySubscription = gql`
 `;
 
 const NewItemList = () => {
-  const {listen: {relatedNode = {}}, loading, error } = useSubscription(mySubscription);
+  const { data, loading, error } = useSubscription(mySubscription);
   if (error) return <p>An error occured when loading a new arrival</p>;
+  if (loading) return <div></div>;
+  let {
+    listen: { relatedNode },
+  } = data;
+
   return (
     <div>
       <h1>New Arrivals!!</h1>
       <StyledList className="new-item-list">
-        {!loading && (
-          <Item
-            id={relatedNode.id}
-            name={relatedNode.name}
-            manufacturerName={relatedNode.manufacturerName}
-            description={relatedNode.description}
-            price={relatedNode.price}
-          ></Item>
-        )}
+        <Item
+          id={relatedNode.id}
+          name={relatedNode.name}
+          manufacturerName={relatedNode.manufacturerName}
+          description={relatedNode.description}
+          price={relatedNode.price}
+        ></Item>
       </StyledList>
     </div>
   );
