329dfa2a4d3d88681830d16111ffa8d9f5af71d2
mtiahrt <mark.tiahrt@outlook.com>
2021-07-01 15:15:23 -0600

    fixed version dep issues

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ItemList from "./Components/ItemList";
 import NavBar from "./Nav/NavBar";
 import NavItem from "./Nav/NavItem";
 import DropdownMenu from "./Nav/DropdownMenu";
-import Facebook from "./Components/Logins/Facebook";
+// import Facebook from "./Components/Logins/Facebook";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Profile from './Components/Profile';
@@ -35,7 +35,8 @@ function App() {
               <header>
                 <h1>Gretchenkelly Shop</h1>
               </header>
-              {userProfile.isLoggedIn ? <ItemList/> : <Facebook/>}
+              <ItemList/>
+              {/* {userProfile.isLoggedIn ? <ItemList/> : <Facebook/>} */}
             </Route>
             <Route path="/Profile">
               <Profile userProfile={userProfile}/>
--- src/Components/Logins/Facebook.js
+++ src/Components/Logins/Facebook.js
@@ -1,24 +1,24 @@
-import React, {useContext} from 'react';
-import FacebookLogin from 'react-facebook-login';
-import { LoginContext } from "../../Contexts/LoginContext"
+// import React, {useContext} from 'react';
+// import FacebookLogin from 'react-facebook-login';
+// import { LoginContext } from "../../Contexts/LoginContext"
 
-export default function Facebook() {
-    const {setUserProfile} = useContext(LoginContext);
+// export default function Facebook() {
+//     const {setUserProfile} = useContext(LoginContext);
 
-    const responseFacebook = response => {
-        setUserProfile({
-            isLoggedIn: true,
-            userID: response.userID,
-            name: response.name,
-            email: response.email,
-            picture: response.picture.data.url
-        });
-    }
-    return (
-    <FacebookLogin
-        appId="1134164680382570"
-        autoLoad={true}
-        fields="name,email,picture"
-        callback={responseFacebook} />
-    )
-}
+//     const responseFacebook = response => {
+//         setUserProfile({
+//             isLoggedIn: true,
+//             userID: response.userID,
+//             name: response.name,
+//             email: response.email,
+//             picture: response.picture.data.url
+//         });
+//     }
+//     return (
+//     <FacebookLogin
+//         appId="1134164680382570"
+//         autoLoad={true}
+//         fields="name,email,picture"
+//         callback={responseFacebook} />
+//     )
+// }
