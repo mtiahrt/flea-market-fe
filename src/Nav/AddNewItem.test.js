8a721374a04ccf2ed2fe5160b161cef1f7278812
mtiahrt <mark.tiahrt@outlook.com>
2021-07-20 14:21:45 -0600

    seperated out unit tests to test 1 thing

--- src/Nav/AddNewItem.test.js
+++ src/Nav/AddNewItem.test.js
@@ -17,7 +17,7 @@ it("renders without crashing", () => {
     ReactDOM.render(<NewItem/>, div);
 });
 
-it("price field allows numbers and only 1 decimal only", () => {
+it("price field allows numbers only", () => {
   //get the price field
     const component = setup();
     const input = component.getByLabelText('price-input')
@@ -28,18 +28,26 @@ it("price field allows numbers and only 1 decimal only", () => {
     expect(input.value).toBe('3');
     fireEvent.change(input, {target: {value: '35'}});
     expect(input.value).toBe('35');
-    //check that decimal is included
-    fireEvent.change(input, {target: {value: '35.'}});
-    expect(input.value).toBe('35.');
-    fireEvent.change(input, {target: {value: '35..'}});
-    expect(input.value).toBe('35.');
-    fireEvent.change(input, {target: {value: '35.wd.'}});
-    expect(input.value).toBe('35.');
     fireEvent.change(input, {target: {value: '35.21'}});
     expect(input.value).toBe('35.21');
     fireEvent.change(input, {target: {value: '35.21a'}});
     expect(input.value).toBe('35.21');
 });
+
+it("price only allows 1 decimal", (() => {
+  //get the price field
+  const component = setup();
+  const input = component.getByLabelText('price-input');
+  //put numbers with 1 decimal
+  fireEvent.change(input, {target: {value: '82'}});
+  expect(input.value).toBe('82');
+  fireEvent.change(input, {target: {value: '82.'}});
+  expect(input.value).toBe('82.');
+  fireEvent.change(input, {target: {value: '82..'}});
+  expect(input.value).toBe('82.');
+  fireEvent.change(input, {target: {value: '82..swd'}});
+  expect(input.value).toBe('82.');
+}));
 
 it("save button disabled when appropriate", () => {
   const component = setup();
@@ -55,16 +63,24 @@ it("save button disabled when appropriate", () => {
   
   const descriptionInput = component.getByLabelText('description-input');
   fireEvent.change(descriptionInput, {target: {value: 'Test description'}});
+  //leave price field empty 
   expect(saveButton).toBeDisabled();
-  
+});
+
+it("save button enabled when appropriate", () => {
+  const component = setup();
+  const saveButton = component.getByLabelText('save-item');
+
+  const titleInput = component.getByLabelText('title-input');
+  fireEvent.change(titleInput, {target: {value: 'Test title'}});
+  const nameInput = component.getByLabelText('name-input');
+  fireEvent.change(nameInput, {target: {value: 'Test name'}});
+  const descriptionInput = component.getByLabelText('description-input');
+  fireEvent.change(descriptionInput, {target: {value: 'Test description'}});
   const priceInput = component.getByLabelText('price-input');
   fireEvent.change(priceInput, {target: {value: '35.21'}});
   //test that the button is enabled
   expect(saveButton).toBeEnabled();
-});
-
-it("save button enabled when appropriate", () => {
-
 });
 
 it("upload image allows only the supported formats", ()=> {
