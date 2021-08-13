c10b7ced06a68afe96bba887deafd391c6452181
mtiahrt <mark.tiahrt@outlook.com>
2021-08-13 09:18:35 -0600

    changed hose url for cypress test

--- cypress/integration/homePage_spec.js
+++ cypress/integration/homePage_spec.js
@@ -1,13 +1,13 @@
 describe('home page e2e test', () => {
     it('Visits the home page', () => {
-        cy.visit("https://localhost:3000");
+        cy.visit("https://flea-market-fe.herokuapp.com");
     });
     it('homepage contains Gretchenkelly title', () => {
-        cy.visit("https://localhost:3000");
+        cy.visit("https://flea-market-fe.herokuapp.com");
         cy.contains('Gretchenkelly');
     });
     it('clicking a sale item takes us to the item detail page', () => {
-        cy.visit("https://localhost:3000");
+        cy.visit("https://flea-market-fe.herokuapp.com");
         cy.contains('Price').click();
         cy.url().should('include', 'DetailedItem/')
     })
