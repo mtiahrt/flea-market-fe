7fe99371c39425c9d88979c66361edffae4ca712
mtiahrt <mark.tiahrt@outlook.com>
2021-09-10 06:42:30 -0600

    refactored Oauths.  Safari doesnt work

--- src/Components/Logins/Oauths.js
+++ src/Components/Logins/Oauths.js
@@ -4,7 +4,6 @@ import { UserProfileContext } from "../../Contexts/LoginContext"
 
 export default function Oauths() {
     const {setUserProfile} = useContext(UserProfileContext);
-
     const popUpWindow = (url, title) => {
         const windowWidth = 500;
         const windowHeight = 500;
@@ -13,46 +12,45 @@ export default function Oauths() {
         const params = `scrollbars=no,resizable=no,status=no,location=yes,toolbar=no,menubar=no,width=${windowWidth}, height=${windowHeight}, top=${yPosition}, left=${xPosition}`
         return window.open(url,title, params);
     }
-    const loginToGoogle = () => {
-        //get the access token
-        new Promise((resolve, reject) => {
-            const googleWindowAuth = popUpWindow(`https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email&include_granted_scopes=true&response_type=token&state=987654321&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2F&client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}`, "Google Authorization")
-            googleWindowAuth.addEventListener("load", () => {
+
+    const extractUrlValue = (key, url) => {
+        const match = url.match('[?&]' + key + '=([^&]+)');
+        return match ? match[1] : null;
+    }
+
+    const addPopUpWindowListener = (urlParameterName, url, providerName) => {
+        return new Promise((resolve, reject) => {
+            const authWindowPopUp = popUpWindow(url, `${providerName} Authorization`)
+            authWindowPopUp.addEventListener("load", () => {
             	try{
-                    debugger;
-            		const urlParams = new URLSearchParams(googleWindowAuth.location.href);
-                    if(urlParams.has('access_token')){
-                        resolve(urlParams.get('access_token'));
-                        googleWindowAuth.close();
+                    const urlParamValue = extractUrlValue(urlParameterName, authWindowPopUp.location.href)
+                    if(urlParamValue){
+                        resolve(urlParamValue);
+                        authWindowPopUp.close();
+                    }else{
+                        throw new Error(`The Url parameter ${urlParameterName} was not found`);
                     }
             	}catch(ex){
-            		reject(`Failed to get a Google access token. Reason: ${ex.message}`);
+            		reject(`Failed to get a ${providerName} access token. Reason: ${ex.message}`);
             	}
             });
-        }).then(accessToken => {
-            axios.get(`https://localhost:8080/userProfile?code=${accessToken}&provider=google`)
-                .then(({data}) => {
-                    setUserProfile(data);
-                });
         })
+    }
+
+    const loginToGoogle = () => {
+        addPopUpWindowListener('access_token', 
+            `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email&include_granted_scopes=true&response_type=token&state=987654321&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2F&client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}`, 'Google')
+            .then(accessToken => {
+                axios.get(`https://localhost:8080/userProfile?code=${accessToken}&provider=google`)
+                    .then(({data}) => {
+                        setUserProfile(data);
+                    });
+            })
     }
     
     const loginToFacebook = () => {
-        //get the FB code
-        new Promise((resolve, reject) => {
-            const facebookWindowAuth = popUpWindow(`https://www.facebook.com/v11.0/dialog/oauth?client_id=${process.env.REACT_APP_FB_CLIENT_ID}&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2F&state=987654321`, "Facebook Authorization")
-            facebookWindowAuth.addEventListener("load", () => {
-            	try{
-            		const urlParams = new URLSearchParams(facebookWindowAuth.location.search);
-                    if(urlParams.has('code')){
-                        resolve(urlParams.get('code'));
-                        facebookWindowAuth.close();
-                    }
-            	}catch(ex){
-            		reject(`Failed to get a Facebook token. Reason: ${ex.message}`);
-            	}
-            });
-        }).then(code => {
+        addPopUpWindowListener('code', `https://www.facebook.com/v11.0/dialog/oauth?client_id=${process.env.REACT_APP_FB_CLIENT_ID}&state=987654321&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2F`, 'Facebook')
+        .then(code => {
             axios.get(`https://localhost:8080/userProfile?code=${code}&provider=facebook`)
                 .then(({data}) => {
                     setUserProfile(data);
@@ -61,15 +59,15 @@ export default function Oauths() {
     }
 
     const loginToTwitter = () => {
-        //go to node endpoint
+        //go to BE node endpoint to get the twitter code.  A different redirect is  
+        //setup for twitter because the popup window didnt work for this provider 
         axios.post(`https://localhost:8080/twitter/requestToken`).then(({data}) => {
             window.location=`https://api.twitter.com/oauth/authenticate?oauth_token=${data.oauth_token}`
-    })
+        })
     }
 
     return (
         <div>
-            {/* {setUserProfile.isLoggedIn && <button onClick={loginToFacebook}>Login User Facebook</button>} */}
             <button onClick={loginToFacebook}>Login in using Facebook</button><br />
             <img alt='Login with Twitter' onClick={loginToTwitter} src="https://cdn.cms-twdigitalassets.com/content/dam/developer-twitter/auth-docs/sign-in-with-twitter-gray.png.twimg.1920.png" className="b04__img b04__img-cover b04__img--fixed is-aligned-left"/><br></br>
             <button onClick={loginToGoogle}>Login in using Google</button><br />
