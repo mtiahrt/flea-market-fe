00905693be589e262c62b533e111cdf913106e82
derek williams <derek61@gmail.com>
2022-08-19 15:06:12 -0600

    added simple test

--- cypress/e2e/homePage_spec_local.cy.js
+++ cypress/e2e/homePage_spec_local.cy.js
@@ -10,4 +10,16 @@ describe('home page e2e test', () => {
     it('check for number of items greater than 0', () => {
       cy.get('.sale-item').should('have.length.greaterThan', 1)
     })
+    
+    it('check for number of sale items is greater than 0', () => {
+      cy.get('.sale-item').should('have.length.greaterThan', 1)
+    })
+
+    it('check for clicking on the first sale-item', () => {
+
+      cy.get('.sale-item').first().as('firstBtn')
+
+      cy.get('@firstBtn').click()
+    })
+
 })
