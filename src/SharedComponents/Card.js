50558c08dbc8cadfdca8e989ff6e56cc4dc6c664
derek williams <derek61@gmail.com>
2022-08-19 13:12:03 -0600

    improved dynamic behavior of grid

--- src/SharedComponents/Card.js
+++ src/SharedComponents/Card.js
@@ -5,7 +5,7 @@ export const CardWrapper = styled.div`
   overflow: hidden;
   padding: 0 0 32px;
   margin: 0;
-  width: 300px;
+  min-width: 300px;
   font-family: Quicksand, arial, sans-serif;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.05), 0 0px 40px rgba(0, 0, 0, 0.08);
   border-radius: 5px;
