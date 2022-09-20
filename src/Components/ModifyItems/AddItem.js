6b5d3ca27c2922a71d35da453d546a0471a0cf10
Mark Tiahrt <mark.tiahrt@outlook.com>
2022-09-20 12:25:58 -0600

    removed mui grid. flexbox instead

--- src/Components/ModifyItems/AddItem.js
+++ src/Components/ModifyItems/AddItem.js
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
 import { Button, InputAdornment, InputLabel, MenuItem, Select, TextareaAutosize } from '@mui/material';
 import styled from 'styled-components';
 import { useLazyQuery, useMutation, useQuery } from '@apollo/client';
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import { GET_CATEGORIES, GET_SUBCATEGORIES, ADD_SALE_ITEM, ADD_ITEM_IMAGE } from '../../queries/graphQL';
 import PreviewImages from '../../SharedComponents/PreviewImages';
@@ -45,7 +43,7 @@ export default function AddItem() {
             fileDataURL: data?.map(item => item.data.createItemImage.itemImage)
           }
         });
-      })
+      });
     });
   };
 
@@ -71,104 +69,85 @@ export default function AddItem() {
   console.log('Add Item is rendering...');
   if (loadingCategories) return 'Loading...';
   if (errorCategories) return `Error! ${errorCategories.message}`;
+
   return (
     <StyledForm onSubmit={handleSubmit(handleNewItemSubmit, onError)}>
       <Typography variant='h4' gutterBottom>New Sale Item</Typography>
-      <Grid
-        container
-        spacing={3}>
-        <Grid item xs={12}>
-          <TextField
-            {...register('name', { required: true })}
-            id='name'
-            label='Name'
-            fullWidth
-            autoComplete='name'
-            variant='standard' />
-          {errors.name?.type === 'required' && 'Name is required'}
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            {...register('manufacturerName', { required: true })}
-            id='manufacturerName'
-            label='Manufacturer Name'
-            fullWidth
-            autoComplete='manufacturer-name'
-            variant='standard'
-          />
-          {errors.manufacturerName?.type === 'required' && 'Manufacturer name is required'}
-        </Grid>
-        <Grid item xs={12}>
-          <InputLabel id='category-select-label'>Category</InputLabel>
-          <Select
-            fullWidth
-            labelId='category-select-label'
-            value={category}
-            label='Category'
-            onChange={handleCategorySelectChange}
-          >
-            {dataCategories.categoriesList.map((category) => (
-              <MenuItem key={category.id} value={category.id}>{category.name}</MenuItem>))}
-          </Select>
-        </Grid>
-        {dataSubs?.category?.subcategoriesList && <Grid item xs={12}>
-          <InputLabel id='subcategory-select-label'>Subcategory</InputLabel>
-          <Select
-            {...register('subcategoryId', { required: true })}
-            fullWidth
-            labelId='subcategory-select-label'
-            value={subcategory}
-            label='Subcategory'
-            onChange={handleSubcategorySelectChange}
-          >
-            {dataSubs.category.subcategoriesList.map(sub => (
-              <MenuItem key={sub.id} value={sub.id}>{sub.name}</MenuItem>))
-            }
-          </Select>
-        </Grid>}
-        <Grid item xs={12}>
-          <TextareaAutosize
-            {...register('description')}
-            id='description'
-            label='Description'
-            autoComplete='description'
-            variant='standard'
-            aria-label='minimum height'
-            minRows={8}
-            placeholder='Item Description'
-            style={{ width: '100%' }}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            {...register('price', { required: true })}
-            type='number'
-            id='price'
-            label='Price'
-            fullWidth
-            autoComplete='price'
-            variant='standard'
-            InputProps={{
-              startAdornment: <InputAdornment position='start'>$</InputAdornment>
-            }}
-          />
-          {errors.price?.type === 'required' && 'Price is required'}
-        </Grid>
-      </Grid>
-      <Grid item style={{ marginTop: '5%' }} xs={12}>
-        <PreviewImages />
-      </Grid>
-      <Grid style={{ marginTop: '5%' }} container spacing={4}>
-        <Grid item xs={4}>
-          <Button type='submit' variant='contained'>Submit</Button>
-        </Grid>
-        <Grid item xs={4}>
-          <Link to={`/`}><Button variant='contained'>Cancel</Button></Link>
-        </Grid>
-      </Grid>
+      <TextField
+        {...register('name', { required: true })}
+        id='name'
+        label='Name'
+        // fullWidth
+        autoComplete='name'
+        variant='standard' />
+      {errors.name?.type === 'required' && 'Name is required'}
+      <TextField
+        {...register('manufacturerName', { required: true })}
+        id='manufacturerName'
+        label='Manufacturer Name'
+        // fullWidth
+        autoComplete='manufacturer-name'
+        variant='standard'
+      />
+      {errors.manufacturerName?.type === 'required' && 'Manufacturer name is required'}
+      <InputLabel id='category-select-label'>Category</InputLabel>
+      <Select
+        // fullWidth
+        labelId='category-select-label'
+        value={category}
+        label='Category'
+        onChange={handleCategorySelectChange}
+      >
+        {dataCategories.categoriesList.map((category) => (
+          <MenuItem key={category.id} value={category.id}>{category.name}</MenuItem>))}
+      </Select>
+      {dataSubs?.category?.subcategoriesList && <>
+        <InputLabel id='subcategory-select-label'>Subcategory</InputLabel>
+        <Select
+          {...register('subcategoryId', { required: true })}
+          // fullWidth
+          labelId='subcategory-select-label'
+          value={subcategory}
+          label='Subcategory'
+          onChange={handleSubcategorySelectChange}
+        >
+          {dataSubs.category.subcategoriesList.map(sub => (
+            <MenuItem key={sub.id} value={sub.id}>{sub.name}</MenuItem>))
+          }
+        </Select>
+      </>}
+      <TextareaAutosize
+        {...register('description')}
+        id='description'
+        label='Description'
+        autoComplete='description'
+        variant='standard'
+        aria-label='minimum height'
+        minRows={8}
+        placeholder='Item Description'
+      />
+      <TextField
+        {...register('price', { required: true })}
+        type='number'
+        id='price'
+        label='Price'
+        autoComplete='price'
+        variant='standard'
+        InputProps={{
+          startAdornment: <InputAdornment position='start'>$</InputAdornment>
+        }}
+      />
+      {errors.price?.type === 'required' && 'Price is required'}
+      <PreviewImages />
+      <Button type='submit' variant='contained'>Submit</Button>
+      <Button onClick={() => history.push(`/`)} style={{ backgroundColor: '#B8BDBB' }}
+              variant='contained'>Cancel</Button>
     </StyledForm>);
 }
 
 const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
   margin: 0 20%;
 `;
--- src/Components/ModifyItems/EditItem.js
+++ src/Components/ModifyItems/EditItem.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import { useForm, useFormState } from 'react-hook-form';
-import { Link, useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { useHistory, useParams } from 'react-router-dom';
 import { Button, InputAdornment, InputLabel, MenuItem, Select, TextareaAutosize } from '@mui/material';
 import styled from 'styled-components';
 import { useLazyQuery, useMutation, useQuery } from '@apollo/client';
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
+
 import {
   GET_SUBCATEGORIES,
   ADD_ITEM_IMAGE,
@@ -19,6 +19,7 @@ import { useLocation } from 'react-router';
 import { saveImages, saveItemImage, saveSaleItem } from './Utilities';
 
 const EditItem = () => {
+  const history = useHistory();
   const { id } = useParams();
   const location = useLocation();
   const { fileDataURL } = location.state
@@ -81,110 +82,90 @@ const EditItem = () => {
   return (
     <StyledForm onSubmit={handleSubmit(handleEditItemSubmit, onError)}>
       <Typography variant='h4' gutterBottom>Edit Sale Item</Typography>
-      <Grid
-        container
-        spacing={3}>
-        <Grid item xs={12}>
-          <TextField
-            {...register('name', { required: true })}
-            defaultValue={dataSaleItem.saleItem.name}
-            id='name'
-            label='Name'
-            fullWidth
-            autoComplete='name'
-            variant='standard' />
-          {errors.name?.type === 'required' && 'Name is required'}
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            {...register('manufacturerName', { required: true })}
-            defaultValue={dataSaleItem.saleItem.manufacturerName}
-            id='manufacturerName'
-            label='Manufacturer Name'
-            fullWidth
-            autoComplete='manufacturer-name'
-            variant='standard'
-          />
-          {errors.manufacturerName?.type === 'required' && 'Manufacturer name is required'}
-        </Grid>
-        <Grid item xs={12}>
-          <InputLabel id='category-select-label'>Category</InputLabel>
-          <Select
-            fullWidth
-            labelId='category-select-label'
-            value={categoryId ? categoryId : dataSaleItem.saleItem.subcategory.category.id
-            }
-            label='Category'
-            onChange={handleCategorySelectChange}
-          >
-            {dataSaleItem.categoriesList.map(category => (
-              <MenuItem key={category.id} value={category.id}>{category.name}</MenuItem>))}
-          </Select>
-        </Grid>
-        <Grid item xs={12}>
-          <InputLabel id='subcategory-select-label'>Subcategory</InputLabel>
-          <Select
-            {...register('subcategoryId', { required: true })}
-            fullWidth
-            labelId='subcategory-select-label'
-            value={subcategory ? subcategory : dataSaleItem.saleItem.subcategory.id}
-            label='Subcategory'
-            onChange={handleSubcategoryOnChange}
-          >
-            {dataSubcategories
-              ? dataSubcategories.category.subcategoriesList.map(sub => (
-                <MenuItem key={sub.id} value={sub.id}>{sub.name}</MenuItem>))
-              : dataSaleItem.saleItem.subcategory.category.subcategoriesList?.map(sub => (
-                <MenuItem key={sub.id} value={sub.id}>{sub.name}</MenuItem>))
-            }
-          </Select>
-        </Grid>
-        <Grid item xs={12}>
-          <TextareaAutosize
-            {...register('description')}
-            defaultValue={dataSaleItem.saleItem.description}
-            id='description'
-            label='Description'
-            autoComplete='description'
-            variant='standard'
-            aria-label='minimum height'
-            minRows={8}
-            placeholder='Item Description'
-            style={{ width: '100%' }}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            defaultValue={dataSaleItem.saleItem.price}
-            {...register('price', { required: true })}
-            type='number'
-            id='price'
-            label='Price'
-            fullWidth
-            autoComplete='price'
-            variant='standard'
-            InputProps={{
-              startAdornment: <InputAdornment position='start'>$</InputAdornment>
-            }}
-          />
-          {errors.price?.type === 'required' && 'Price is required'}
-        </Grid>
-      </Grid>
-      <Grid item style={{ marginTop: '5%' }} xs={12}>
-        <PreviewImages fileDataURL={fileDataURL} />
-      </Grid>
-      <Grid style={{ marginTop: '5%' }} container spacing={4}>
-        <Grid item xs={4}>
-          <Button type='submit' variant='contained'>Submit</Button>
-        </Grid>
-        <Grid item xs={4}>
-          <Link to={`/DetailedItem/${saleId}`}><Button variant='contained'>Cancel</Button></Link>
-        </Grid>
-      </Grid>
+      <TextField
+        {...register('name', { required: true })}
+        defaultValue={dataSaleItem.saleItem.name}
+        id='name'
+        label='Name'
+        fullWidth
+        autoComplete='name'
+        variant='standard' />
+      {errors.name?.type === 'required' && 'Name is required'}
+      <TextField
+        {...register('manufacturerName', { required: true })}
+        defaultValue={dataSaleItem.saleItem.manufacturerName}
+        id='manufacturerName'
+        label='Manufacturer Name'
+        fullWidth
+        autoComplete='manufacturer-name'
+        variant='standard'
+      />
+      {errors.manufacturerName?.type === 'required' && 'Manufacturer name is required'}
+      <InputLabel id='category-select-label'>Category</InputLabel>
+      <Select
+        fullWidth
+        labelId='category-select-label'
+        value={categoryId ? categoryId : dataSaleItem.saleItem.subcategory.category.id
+        }
+        label='Category'
+        onChange={handleCategorySelectChange}
+      >
+        {dataSaleItem.categoriesList.map(category => (
+          <MenuItem key={category.id} value={category.id}>{category.name}</MenuItem>))}
+      </Select>
+      <InputLabel id='subcategory-select-label'>Subcategory</InputLabel>
+      <Select
+        {...register('subcategoryId', { required: true })}
+        fullWidth
+        labelId='subcategory-select-label'
+        value={subcategory ? subcategory : dataSaleItem.saleItem.subcategory.id}
+        label='Subcategory'
+        onChange={handleSubcategoryOnChange}
+      >
+        {dataSubcategories
+          ? dataSubcategories.category.subcategoriesList.map(sub => (
+            <MenuItem key={sub.id} value={sub.id}>{sub.name}</MenuItem>))
+          : dataSaleItem.saleItem.subcategory.category.subcategoriesList?.map(sub => (
+            <MenuItem key={sub.id} value={sub.id}>{sub.name}</MenuItem>))
+        }
+      </Select>
+      <TextareaAutosize
+        {...register('description')}
+        defaultValue={dataSaleItem.saleItem.description}
+        id='description'
+        label='Description'
+        autoComplete='description'
+        variant='standard'
+        aria-label='minimum height'
+        minRows={8}
+        placeholder='Item Description'
+        style={{ width: '100%' }}
+      />
+      <TextField
+        defaultValue={dataSaleItem.saleItem.price}
+        {...register('price', { required: true })}
+        type='number'
+        id='price'
+        label='Price'
+        fullWidth
+        autoComplete='price'
+        variant='standard'
+        InputProps={{
+          startAdornment: <InputAdornment position='start'>$</InputAdornment>
+        }}
+      />
+      {errors.price?.type === 'required' && 'Price is required'}
+      <PreviewImages fileDataURL={fileDataURL} />
+      <Button type='submit' variant='contained'>Submit</Button>
+      <Button onClick={() => history.push(`/DetailedItem/${saleId}`)} style={{ backgroundColor: '#B8BDBB' }}
+              variant='contained'>Cancel</Button>
     </StyledForm>
   );
 };
 const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
   margin: 0 20%;
 `;
 export default EditItem;
