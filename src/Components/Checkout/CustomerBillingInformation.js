79001a88db3edcdf416a985083f5a656c33b6f9e
Mark Tiahrt <mark.tiahrt@outlook.com>
2022-10-11 14:32:15 -0600

    cart screen css partical complete

--- src/Components/Checkout/CustomerBillingInformation.js
+++ src/Components/Checkout/CustomerBillingInformation.js
@@ -45,145 +45,147 @@ export default function CustomerBillingInformation() {
   const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
   return (
-    <StyledForm onSubmit={handleSubmit(handlePaymentSubmit, onError)}>
+    <>
       <Typography variant='h4' gutterBottom>Customer Information</Typography>
-      <TextField
-        {...register('firstName', { required: true })}
-        id='firstName'
-        label='First Name'
-        autoComplete='first-name'
-        variant='standard' />
-      {errors.name?.type === 'required' && 'Name is required'}
-      <TextField
-        {...register('lastName', { required: true })}
-        id='lastName'
-        label='Last Name'
-        autoComplete='last-name'
-        variant='standard'
-      />
-      <TextField
-        {...register('email', { required: true })}
-        id='email'
-        label='Email'
-        autoComplete='email'
-        variant='standard'
-      />
-      <TextField
-        {...register('address', { required: true })}
-        id='address'
-        label='Address'
-        autoComplete='address'
-        variant='standard'
-      />
-      <TextField
-        {...register('city', { required: true })}
-        id='city'
-        label='City'
-        autoComplete='city'
-        variant='standard'
-      />
-      <InputLabel id='state-select-label'>State</InputLabel>
-      <Select
-        {...register('state', { required: true })}
-        labelId='state-select-label'
-        label='State'
-        onChange={handleStateSelectChange}
-      >
-        <MenuItem value='CO'>CO</MenuItem>
-        {/*{dataSubs.category.subcategoriesList.map(sub => (*/}
-        {/*  <MenuItem key={sub.id} value={sub.id}>{sub.name}</MenuItem>))*/}
-        {/*}*/}
-      </Select>
-      <TextField
-        {...register('zip', { required: true })}
-        id='zip'
-        label='Zip'
-        autoComplete='zip'
-        variant='standard'
-      />
-      <Typography variant='h4' gutterBottom>Payment Info</Typography>
-      {/*<PaymentForm/>*/}
-      <StyledPaymentDiv>
-        <StyledPaymentForm>
-          <h2>Credit Card</h2>
-          <CardElement className="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium css-sghohy-MuiButtonBase-root-MuiButton-root" />
-          <Button>Pay now</Button>
-        </StyledPaymentForm>
-      </StyledPaymentDiv>
-      <label>Same as Shipping address</label>
-      <Checkbox {...label} defaultChecked />
-      <Typography variant='h4' gutterBottom>Billing Address</Typography>
-      <TextField
-        {...register('billingFirstName', { required: true })}
-        id='billingFirstName'
-        label='First Name'
-        autoComplete='first-name'
-        variant='standard' />
-      {errors.name?.type === 'required' && 'Billing name is required'}
-      <TextField
-        {...register('billingLastName', { required: true })}
-        id='billingLastName'
-        label='Last Name'
-        autoComplete='last-name'
-        variant='standard'
-      />
-      <TextField
-        {...register('billingAddress', { required: true })}
-        id='billingAddress'
-        label='Address'
-        autoComplete='address'
-        variant='standard'
-      />
-      <TextField
-        {...register('billingCity', { required: true })}
-        id='billingCity'
-        label='City'
-        autoComplete='city'
-        variant='standard'
-      />
-      <InputLabel id='state-select-label'>State</InputLabel>
-      <Select
-        {...register('billingState', { required: true })}
-        labelId='billing-state-select-label'
-        label='State'
-        onChange={handleStateSelectChange}
-      >
-        <MenuItem value='CO'>CO</MenuItem>
-        {/*{dataSubs.category.subcategoriesList.map(sub => (*/}
-        {/*  <MenuItem key={sub.id} value={sub.id}>{sub.name}</MenuItem>))*/}
-        {/*}*/}
-      </Select>
-      <TextField
-        {...register('billingZip', { required: true })}
-        id='billingzip'
-        label='Zip'
-        autoComplete='zip'
-        variant='standard'
-      />
-      <Button type='submit' variant='contained'>Complete Payment</Button>
-    </StyledForm>
+      <StyledForm onSubmit={handleSubmit(handlePaymentSubmit, onError)}>
+        <TextField style={{flexGrow: '1', flexShrink: '1'}}
+          {...register('firstName', { required: true })}
+          id='firstName'
+          label='First Name'
+          autoComplete='first-name'
+          variant='standard' />
+        {errors.name?.type === 'required' && 'Name is required'}
+        <TextField style={{flexGrow: '1', flexShrink: '1'}}
+          {...register('lastName', { required: true })}
+          id='lastName'
+          label='Last Name'
+          autoComplete='last-name'
+          variant='standard'
+        />
+        <TextField style={{flexGrow: '1', flexShrink: '1'}}
+          {...register('email', { required: true })}
+          id='email'
+          label='Email'
+          autoComplete='email'
+          variant='standard'
+        />
+        <TextField style={{flexGrow: '3', flexShrink: '1', width: '100%'}}
+          {...register('address', { required: true })}
+          id='address'
+          label='Address'
+          autoComplete='address'
+          variant='standard'
+        />
+        <TextField style={{flexGrow: '1', flexShrink: '1'}}
+          {...register('city', { required: true })}
+          id='city'
+          label='City'
+          autoComplete='city'
+          variant='standard'
+        />
+        {/*<InputLabel id='state-select-label'>State</InputLabel>*/}
+        <Select style={{flexGrow: '1', flexShrink: '1'}}
+          {...register('state', { required: true })}
+          labelId='state-select-label'
+          label='State'
+          onChange={handleStateSelectChange}
+        >
+
+          <MenuItem value='CO'>CO</MenuItem>
+          {/*{dataSubs.category.subcategoriesList.map(sub => (*/}
+          {/*  <MenuItem key={sub.id} value={sub.id}>{sub.name}</MenuItem>))*/}
+          {/*}*/}
+        </Select>
+
+        <TextField style={{flexGrow: '1', flexShrink: '1'}}
+          {...register('zip', { required: true })}
+          id='zip'
+          label='Zip'
+          autoComplete='zip'
+          variant='standard'
+        />
+        <StyledPaymentDiv>
+          <StyledPaymentForm>
+            <h2>Credit Card</h2>
+            <CardElement />
+            <Button>Pay now</Button>
+          </StyledPaymentForm>
+        </StyledPaymentDiv>
+        <Typography variant='h4' gutterBottom>Billing Address</Typography>
+          <label>Same as Shipping address</label>
+          <Checkbox {...label} defaultChecked />
+
+          <TextField
+            {...register('billingFirstName', { required: true })}
+            id='billingFirstName'
+            label='First Name'
+            autoComplete='first-name'
+            variant='standard' />
+          {errors.name?.type === 'required' && 'Billing name is required'}
+          <TextField
+            {...register('billingLastName', { required: true })}
+            id='billingLastName'
+            label='Last Name'
+            autoComplete='last-name'
+            variant='standard'
+          />
+          <TextField
+            {...register('billingAddress', { required: true })}
+            id='billingAddress'
+            label='Address'
+            autoComplete='address'
+            variant='standard'
+          />
+          <TextField
+            {...register('billingCity', { required: true })}
+            id='billingCity'
+            label='City'
+            autoComplete='city'
+            variant='standard'
+          />
+          <InputLabel id='state-select-label'>State</InputLabel>
+          <Select
+            {...register('billingState', { required: true })}
+            labelId='billing-state-select-label'
+            label='State'
+            onChange={handleStateSelectChange}
+          >
+            <MenuItem value='CO'>CO</MenuItem>
+            {/*{dataSubs.category.subcategoriesList.map(sub => (*/}
+            {/*  <MenuItem key={sub.id} value={sub.id}>{sub.name}</MenuItem>))*/}
+            {/*}*/}
+          </Select>
+          <TextField
+            {...register('billingZip', { required: true })}
+            id='billingzip'
+            label='Zip'
+            autoComplete='zip'
+            variant='standard'
+          />
+          <Button type='submit' variant='contained'>Complete Payment</Button>
+      </StyledForm>
+    </>
   );
 }
 
 const StyledForm = styled.form`
+  border: solid #32a1ce;
+  padding: 5px;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  width: 55%;
+  flex-wrap: wrap;
   gap: 1rem;
-  margin: 0 10%;
+  margin: 0 5%;
 `;
 
 
 const StyledPaymentDiv = styled.div`
-  height: 300px;
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  flex-direction: row;
   gap: 1rem;
-  margin: 0 10%;
 `;
 const StyledPaymentForm = styled.form`
-  height: 100px;
   min-width: 500px;
 `;
