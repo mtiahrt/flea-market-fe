e3cdee5150d73bb9ffdebcf9d3fd8758ec360d5f
Mark Tiahrt <mark.tiahrt@outlook.com>
2022-10-12 10:43:18 -0600

    more css cleanup

--- src/Components/Checkout/CustomerBillingInformation.js
+++ src/Components/Checkout/CustomerBillingInformation.js
@@ -3,13 +3,15 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
-import { Button, InputLabel, Select, Checkbox, MenuItem, FormControlLabel, FormControl } from '@mui/material';
+import { Button, InputLabel, Select, Checkbox, MenuItem, FormControlLabel, FormControl, Divider } from '@mui/material';
 import axios from 'axios';
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import states from '../../assets/data/states.json'
+import states from '../../assets/data/states.json';
+import { useState } from 'react';
 
 export default function CustomerBillingInformation() {
   const { register, formState: { errors }, handleSubmit } = useForm();
+  const {billingState, setBillingState} = useState();
   const stripe = useStripe();
   const elements = useElements();
   const handlePaymentSubmit = async (paymentData, e) => {
@@ -38,75 +40,73 @@ export default function CustomerBillingInformation() {
     console.error('Error in form submission', e.state);
   };
 
-  function handleStateSelectChange() {
-
+  function handleStateSelectChange(e) {
+    console.log(e)
   }
 
   const getStates = () => (states.map(state => (
-    <MenuItem key={state.id} value={state.id}>{state.name}</MenuItem>)))
+    <MenuItem key={`stateKey${state.id}`} data-abbr={state.abbr} value={state.id}>{state.name}</MenuItem>))
+  );
 
-  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
-
-  console.log('states is', states);
   return (
     <>
       <Typography variant='h4' gutterBottom>Customer Information</Typography>
       <StyledForm onSubmit={handleSubmit(handlePaymentSubmit, onError)}>
-        <TextField style={{flexGrow: '1', flexShrink: '1'}}
-          {...register('firstName', { required: true })}
-          id='firstName'
-          label='First Name'
-          autoComplete='first-name'
-          variant='standard' />
+        <TextField style={{ flexGrow: '1', flexShrink: '1' }}
+                   {...register('firstName', { required: true })}
+                   id='firstName'
+                   label='First Name'
+                   autoComplete='first-name'
+                   variant='standard' />
         {errors.name?.type === 'required' && 'Name is required'}
-        <TextField style={{flexGrow: '1', flexShrink: '1'}}
-          {...register('lastName', { required: true })}
-          id='lastName'
-          label='Last Name'
-          autoComplete='last-name'
-          variant='standard'
+        <TextField style={{ flexGrow: '1', flexShrink: '1' }}
+                   {...register('lastName', { required: true })}
+                   id='lastName'
+                   label='Last Name'
+                   autoComplete='last-name'
+                   variant='standard'
         />
-        <TextField style={{flexGrow: '1', flexShrink: '1'}}
-          {...register('email', { required: true })}
-          id='email'
-          label='Email'
-          autoComplete='email'
-          variant='standard'
+        <TextField style={{ flexGrow: '1', flexShrink: '1' }}
+                   {...register('email', { required: true })}
+                   id='email'
+                   label='Email'
+                   autoComplete='email'
+                   variant='standard'
         />
-        <TextField style={{flexGrow: '3', flexShrink: '1', width: '100%'}}
-          {...register('address', { required: true })}
-          id='address'
-          label='Address'
-          autoComplete='address'
-          variant='standard'
+        <TextField style={{ flexGrow: '3', flexShrink: '1', width: '100%' }}
+                   {...register('address', { required: true })}
+                   id='address'
+                   label='Address'
+                   autoComplete='address'
+                   variant='standard'
         />
-        <TextField style={{flexGrow: '1', flexShrink: '1'}}
-          {...register('city', { required: true })}
-          id='city'
-          label='City'
-          autoComplete='city'
-          variant='standard'
+        <TextField style={{ flexGrow: '1', flexShrink: '1' }}
+                   {...register('city', { required: true })}
+                   id='city'
+                   label='City'
+                   autoComplete='city'
+                   variant='standard'
         />
-        
-        <FormControl  style={{flexGrow: '1', flexShrink: '1'}}>
-          <InputLabel id="demo-simple-select-label">State</InputLabel>
-            <Select
-              {...register('state', { required: true })}
-              labelId='state-select-label'
-              label='State'
-              onChange={handleStateSelectChange}
-            >
-            {states.map(state => (
-              <MenuItem key={state.id} value={state.id}>{state.name}</MenuItem>))}
+
+        <FormControl style={{ flexGrow: '1', flexShrink: '1' }}>
+          <InputLabel id='demo-simple-select-label'>State</InputLabel>
+          <Select
+            {...register('state', { required: true })}
+            labelId='state-select-label'
+            label='State'
+            // onChange={handleStateSelectChange}
+          >
+            {getStates()}
           </Select>
         </FormControl>
-        <TextField style={{flexGrow: '1', flexShrink: '1'}}
-          {...register('zip', { required: true })}
-          id='zip'
-          label='Zip'
-          autoComplete='zip'
-          variant='standard'
+        <TextField style={{ flexGrow: '1', flexShrink: '1' }}
+                   {...register('zip', { required: true })}
+                   id='zip'
+                   label='Zip'
+                   autoComplete='zip'
+                   variant='standard'
         />
+        <Divider style={{ marginTop: '3%', flexGrow: '1', flexShrink: '1', width: '100%'}} />
         <StyledPaymentDiv>
           <StyledPaymentForm>
             <h2>Credit Card</h2>
@@ -114,63 +114,64 @@ export default function CustomerBillingInformation() {
             <Button>Pay now</Button>
           </StyledPaymentForm>
         </StyledPaymentDiv>
-        <Typography variant='h4' gutterBottom>Billing Address</Typography>
-          <FormControlLabel control={<Checkbox defaultChecked />} label="Same as Shipping address" />
-          <TextField
-            {...register('billingFirstName', { required: true })}
-            id='billingFirstName'
-            label='First Name'
-            autoComplete='first-name'
-            variant='standard' />
-          {errors.name?.type === 'required' && 'Billing name is required'}
-          <TextField
-            {...register('billingLastName', { required: true })}
-            id='billingLastName'
-            label='Last Name'
-            autoComplete='last-name'
-            variant='standard'
-          />
-          <TextField
-            {...register('billingAddress', { required: true })}
-            id='billingAddress'
-            label='Address'
-            autoComplete='address'
-            variant='standard'
-          />
-          <TextField
-            {...register('billingCity', { required: true })}
-            id='billingCity'
-            label='City'
-            autoComplete='city'
-            variant='standard'
-          />
-          <FormControl  style={{flexGrow: '1', flexShrink: '1'}}>
-            <InputLabel id="demo-simple-select-label">State</InputLabel>
-            <Select
+        <Divider style={{ flexGrow: '1', flexShrink: '1', width: '100%'}} />
+        <Typography style={{ margin: '10px 0px 0px 0px', width: '100%'}} variant='h4' gutterBottom>Billing Address</Typography>
+        <FormControlLabel style={{width: '100%'}} control={<Checkbox defaultChecked />} label='Same as Shipping address' />
+        <TextField style={{ flexGrow: '1', flexShrink: '1' }}
+          {...register('billingFirstName', { required: true })}
+          id='billingFirstName'
+          label='First Name'
+          autoComplete='first-name'
+          variant='standard' />
+        {errors.name?.type === 'required' && 'Billing name is required'}
+        <TextField style={{ flexGrow: '1', flexShrink: '1',  width: '50%'}}
+          {...register('billingLastName', { required: true })}
+          id='billingLastName'
+          label='Last Name'
+          autoComplete='last-name'
+          variant='standard'
+        />
+        <TextField style={{ flexGrow: '1', flexShrink: '1', width: '100%'}}
+          {...register('billingAddress', { required: true })}
+          id='billingAddress'
+          label='Address'
+          autoComplete='address'
+          variant='standard'
+        />
+        <TextField style={{ flexGrow: '1', flexShrink: '1' }}
+          {...register('billingCity', { required: true })}
+          id='billingCity'
+          label='City'
+          autoComplete='city'
+          variant='standard'
+        />
+        <FormControl style={{ flexGrow: '1', flexShrink: '1' }}>
+          <InputLabel id='demo-simple-select-label'>State</InputLabel>
+          <Select
             {...register('state', { required: true })}
-            id='bullingState'
+            id='billingState'
             label='State'
             // onChange={handleStateSelectChange}
-        >
+          >
 
-          <MenuItem value='CO'>CO</MenuItem>
-        </Select>
+            {getStates()}
+          </Select>
         </FormControl>
-          <TextField
-            {...register('billingZip', { required: true })}
-            id='billingzip'
-            label='Zip'
-            autoComplete='zip'
-            variant='standard'
-          />
-          <Button type='submit' variant='contained'>Complete Payment</Button>
+        <TextField style={{ flexGrow: '1', flexShrink: '1' }}
+          {...register('billingZip', { required: true })}
+          id='billingzip'
+          label='Zip'
+          autoComplete='zip'
+          variant='standard'
+        />
+        <Button type='submit' variant='contained'>Complete Payment</Button>
       </StyledForm>
     </>
   );
 }
 
 const StyledForm = styled.form`
-  border: solid #32a1ce;
+  //border: solid #32a1ce;
   padding: 5px;
   display: flex;
   flex-direction: row;
@@ -181,6 +182,10 @@ const StyledForm = styled.form`
   margin: 0 5%;
 `;
 
+const StyledFieldEqualSize = styled.input`
+  flex-grow: 1;
+  flex-shrink: 1;
+`
 
 const StyledPaymentDiv = styled.div`
   display: flex;
