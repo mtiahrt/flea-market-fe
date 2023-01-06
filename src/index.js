5fef52a7922b46f0da70efef7792de7dbe478b63
Mark Tiahrt <mark.tiahrt@outlook.com>
2023-01-06 13:11:45 -0700

    removed web socket dependencies

--- src/index.js
+++ src/index.js
@@ -2,40 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { ApolloProvider } from '@apollo/client';
-import { WebSocketLink } from '@apollo/client/link/ws';
-import { split, HttpLink } from '@apollo/client';
-import { getMainDefinition } from '@apollo/client/utilities';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Elements } from '@stripe/react-stripe-js';
 import { stripePromise } from './utils/stripe/stripe.utils';
 
-const httpLink = new HttpLink({
-  uri: process.env[`REACT_APP_${process.env.NODE_ENV}_GRAPHQL_END_POINT_URI`],
-});
-
-const wsLink = new WebSocketLink({
-  uri: process.env[`REACT_APP_${process.env.NODE_ENV}_GRAPHQL_WEB_SOCKET_URI`],
-  options: {
-    reconnect: true,
-  },
-});
-
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    );
-  },
-  wsLink,
-  httpLink
-);
-
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: splitLink,
+  uri: process.env[`REACT_APP_${process.env.NODE_ENV}_GRAPHQL_END_POINT_URI`],
 });
 
 ReactDOM.render(
