c74ab9813ddaa4f4e748d7c845bf7dced257d789
Mark Tiahrt <mark.tiahrt@outlook.com>
2023-05-10 11:30:38 -0600

    started cypress setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,27 @@
-const { defineConfig } = require('cypress')
+// const { defineConfig } = require('cypress')
+//
+// module.exports = defineConfig({
+//   e2e: {
+//     // We've imported your old cypress plugins here.
+//     // You may want to clean this up later by importing these.
+//     setupNodeEvents(on, config) {
+//       return require('./cypress/plugins/index.js')(on, config)
+//     },
+//   },
+// })
+const { defineConfig } = require('cypress');
+const cypressFirebasePlugin = require('cypress-firebase').plugin;
+const admin = require('firebase-admin');
 
 module.exports = defineConfig({
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
+    baseUrl: 'http://localhost:3000',
+    // NOTE: Make supportFile exists if separate location is provided
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      // e2e testing node events setup code
+      return cypressFirebasePlugin(on, config, admin);
+      // NOTE: If not setting GCLOUD_PROJECT env variable, project can be set like so:
+      // return cypressFirebasePlugin(on, config, admin, { projectId: 'some-project' });
     },
   },
-})
+});
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,7 +14,26 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
 
 // Alternatively you can use CommonJS syntax:
-// require('./commands')
+// require('./commands');
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+import 'firebase/compat/firestore';
+import { attachCustomCommands } from 'cypress-firebase';
+//
+// const fbConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+// };
+//
+// firebase.initializeApp(fbConfig);
+//
+// attachCustomCommands({ Cypress, cy, firebase });
