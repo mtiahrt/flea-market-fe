10ddcd804231f2b22b06017c4c128df773d5f278
Mark Tiahrt <mark.tiahrt@outlook.com>
2023-07-24 14:24:24 -0600

    added hamburger

--- src/nav/NavItems.js
+++ src/nav/NavItems.js
@@ -11,6 +11,9 @@ import { ReactComponent as SaleIcon } from '../icons/sale-svgrepo-com.svg';
 import { ReactComponent as CategoryIcon } from '../icons/category-svgrepo-com.svg';
 import { ReactComponent as ProfileIcon } from '../icons/profile.svg';
 import { UserContext } from '../contexts/UserContext';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { ReactComponent as HamburgerIcon } from '../icons/menu-hamburger.svg';
 
 function NavItems() {
   const { user, setUser } = useContext(UserContext);
@@ -30,6 +33,20 @@ function NavItems() {
   };
   return (
     <>
+      <NavItem
+        className="hamburger"
+        isActive={active}
+        clickHandler={() =>
+          setActive((prev) => (prev === 'hamburger' ? '' : 'hamburger'))
+        }
+        style={{ marginRight: 'auto' }}
+        icon={<HamburgerIcon name="hamburger" />}
+      ></NavItem>
+      <StyledLiTitle>
+        <Link to={'/#'}>
+          <StyledH1>Upcycle Treasures</StyledH1>
+        </Link>
+      </StyledLiTitle>
       <NavItem
         className="profile-button"
         isActive={active}
@@ -85,5 +102,11 @@ function NavItems() {
     </>
   );
 }
-
+const StyledH1 = styled.h1`
+  font-size: var(--font-size-5);
+  color: var(--logo-fill-color);
+`;
+const StyledLiTitle = styled.li`
+  flex-grow: 0.4;
+`;
 export default NavItems;
--- src/nav/UpperNavBar.js
+++ src/nav/UpperNavBar.js
@@ -2,22 +2,12 @@ import React from 'react';
 import './Navigation.css';
 import styled from 'styled-components';
 import NavItems from './NavItems';
-import { Link } from 'react-router-dom';
-import { ReactComponent as HamburgerIcon } from '../icons/menu-hamburger.svg';
 
 const UpperNavBar = () => {
   return (
     <StyledDiv>
       <StyledNav className="navbar">
         <ul className="navbar-nav">
-          <StyledLi className="nav-item">
-            <HamburgerIcon />
-          </StyledLi>
-          <StyledLiTitle>
-            <Link to={'/#'}>
-              <StyledH1>Upcycle Treasures</StyledH1>
-            </Link>
-          </StyledLiTitle>
           <NavItems />
         </ul>
       </StyledNav>
@@ -32,22 +22,6 @@ const StyledDiv = styled.div`
   @media (max-width: 40em) {
     border-bottom: 5px solid var(--backdrop-color);
   }
-`;
-
-const StyledLi = styled.li`
-  margin-right: auto;
-  display: none;
-  @media (max-width: 40em) {
-    display: block;
-  }
-`;
-const StyledLiTitle = styled.li`
-  flex-grow: 0.4;
-`;
-
-const StyledH1 = styled.h1`
-  font-size: var(--font-size-5);
-  color: var(--logo-fill-color);
 `;
 
 const StyledNav = styled.nav`
